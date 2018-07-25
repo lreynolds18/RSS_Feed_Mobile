fec441c6039189e573f3649da9a8a7358393361d
Lucas Reynolds <lreynolds18@gmail.com>
2018-07-25 13:22:11 -0400

    got code for xml fetch working.

--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class Feed extends React.Component {
+  /*
+   * constructor - 
+   */
+  constructor(props) {
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  /*
+   * componentDidMount - 
+   */
+  componentDidMount() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status === 200) {
+        console.log('success', request.responseText);
+      } else {
+        console.warn('error: ' + request.status);
+      }
+    };
+
+    request.open('GET', 'https://reddit.com/r/datascience.rss');
+    request.send();
+    console.log(request);
+  }
+
+  /*
+   * link_builder - build each view for every link in rss
+   */
+  link_builder(link) {
+    return (
+        <Text>Hello There</Text>
+    );
+  }
+
+  /*
+   * render - render jsx
+   */
   render() {
     return (
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Changes you make will automatically reload.</Text>
+        <Text style={styles.text}>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -16,8 +58,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  }
 });
--- src/Feed.test.js
+++ src/Feed.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import App from './App';
+import Feed from './Feed';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = renderer.create(<Feed />).toJSON();
   expect(rendered).toBeTruthy();
 });
