fd50e0dc1847fa330d3d675ed5c1740355980fec
Lucas Reynolds <lreynolds18@gmail.com>
2018-08-06 17:32:54 -0400

    added tests, started writing tests for feed, added enzyme/enzyme-adapter, cleaned up way to display each rss feed, added error checking for empty asyncstorage

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,8 @@ import {
     AsyncStorage, 
     FlatList, 
 //    StyleSheet,
+    Platform,
+    WebView,
 } from "react-native";
 import {
     Container,
@@ -19,6 +21,13 @@ import { parseString } from "react-native-xml2js";
 
 import Colors from "./Colors";
 
+// var DomParser = require('react-native-html-parser').DOMParser;
+
+// TODO: check if shouldComponentUpdate(nextProps, nextState) can update the 
+//       state when the objects in state are partially changed
+//       (instead of having to redefine an array)
+// TODO: figure out where to put asyncstorage and xml calls
+// TODO: beautifully display text / video
 
 export default class Feed extends Component {
   /*
@@ -37,7 +46,7 @@ export default class Feed extends Component {
    */
   constructor(props) {
       super(props);
-      this.state = { RSS: [], feeds: []} ;
+      this.state = { loaded: false, RSS: [], feeds: []} ;
   }
 
   /*
@@ -55,15 +64,8 @@ export default class Feed extends Component {
       console.log("in mnt");
       AsyncStorage.getItem("feeds")
           .then((value) => {
-              if (value === null) {
-                  // replace with text and button
-                  Alert.alert(
-                      "Error: no RSS feeds available",
-                      "Please enter feeds in settings",
-                      [ {text: "OK"}, ],
-                      { cancelable: false }
-                  );
-              } else {
+              this.setState({ loaded: true });
+              if (value !== null) {
                   value = [...JSON.parse(value)];
                   this.setState({ "RSS": value });
           
@@ -98,8 +100,8 @@ export default class Feed extends Component {
           }
 
           if (request.status === 200) {
-              // console.log('success', request.responseText);
               parseString(request.response, (err, result) => {
+                  console.log(result);
                   let feeds = [...this.state.feeds, ...result.feed.entry];
                   this.setState({ feeds: feeds });
               });
@@ -113,6 +115,13 @@ export default class Feed extends Component {
   }
 
   /*
+   * htmlParser - extracts out any html tags
+   */
+  htmlParser(html) {
+    return html;
+  }
+
+  /*
    * renderRSSFeed - build each view for every link in rss
    */
   renderRSSFeed(item, index) {
@@ -121,14 +130,27 @@ export default class Feed extends Component {
 
       const title = item.title[0];
       let content = item.content[0]['_'];
-      content = content.substring(content.lastIndexOf("<p>") + 3, content.lastIndexOf("</p>"));
+      let htmlStyle = "<style> .div { color: #ffffff } </style>";
+
+      // let result = this.htmlParser(content);
+      // console.log(result);
+      // content = content.substring(content.lastIndexOf("<div class=\"md\">") + 16, content.lastIndexOf("</div>"));
+
       const link = item.link[0].$.href;
 
+      //    <ListItem style={{flex: 1, flexDirection:"column", justifyContent:"center"}}>
+      //        <Text style={{color: Colors.primaryTextColor, flex: 0.1}}>{ link }</Text>
       return (
-          <ListItem style={{flex: 1, flexDirection:"column", justifyContent:"center"}}>
+          <ListItem style={{flex: 1, justifyContent:"center"}}>
               <Text style={{color: Colors.primaryTextColor, flex: 0.1}}>{ title }</Text>
-              <Text style={{color: Colors.primaryTextColor, flex: 0.8}}>{ content }</Text>
-              <Text style={{color: Colors.primaryTextColor, flex: 0.1}}>{ link }</Text>
+              <View style={{flex: 0.9}}>
+                  <WebView
+                    style={{flex: 0.9}}
+                    source={{ html: content }} 
+                    scalesPageToFit={ true }
+                    scrollEnabled={ false }
+                  />
+              </View>
           </ListItem>
       );
   }
@@ -137,8 +159,15 @@ export default class Feed extends Component {
    * Generate List of RSS Feeds 
    */
   renderBody() {
-      if (this.state.feeds.length === 0) {
+      if (this.state.feeds.length === 0 && this.state.loaded === false) {
           return ( <Spinner color='gray' /> );
+      } else if (this.state.feeds.length === 0 && this.state.loaded === true) {
+          return ( 
+              <Text 
+                style={{justifyContent: "center", color: Colors.primaryTextColor}}>
+                  Enter some RSS feeds to get started!
+              </Text>
+          );
       } else {
           return (
               <FlatList
@@ -158,10 +187,10 @@ export default class Feed extends Component {
    */
   render() {
       console.log(this.state);
+      // style={{backgroundColor: Colors.backgroundColor}}
       return (
           <Container>
               <Content 
-                  style={{backgroundColor: Colors.backgroundColor}}
                   contentContainerstyle={{
                       flex: 1, 
                       flexDirection:"column", 
--- src/Feed.test.js
+++ src/Feed.test.js
@@ -1,8 +1,19 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {shallow} from 'enzyme';
 import Feed from "./Feed";
 
-it("renders without crashing", () => {
-    const rendered = renderer.create(<Feed />).toJSON();
-    expect(rendered).toBeTruthy();
+describe("", () => {
+    it("renders without crashing", () => {
+        const rendered = renderer.create(<Feed />).toJSON();
+        expect(rendered).toBeTruthy();
+    });
+});
+
+describe("Test htmlParser logic", () => {
+    it("test parser on single comment", () => {
+        content = "<!-- SC_OFF --><div class=\"md\"><p>I&#39;ve created a series of tutorials on how to use NEAT (Neural Evolution of Augmenting Topologies) with Open-AI&#39;s Retro library to train a network to play basically any retro game. How well it does depends on lots of factors. I&#39;ve personally had success with Donkey Kong Country and Sonic the Hedgehog 2. </p> <p>Here&#39;s the tutorial: <a href=\"https://www.youtube.com/watch?v=CFa6NhLgeL0&amp;list=PLTWFMbPFsvz3CeozHfeuJIXWAJMkPtAdS\">https://www.youtube.com/watch?v=CFa6NhLgeL0&amp;list=PLTWFMbPFsvz3CeozHfeuJIXWAJMkPtAdS</a></p> <p>Repo: <a href=\"https://gitlab.com/lucasrthompson/Sonic-Bot-In-OpenAI-and-NEAT\">https://gitlab.com/lucasrthompson/Sonic-Bot-In-OpenAI-and-NEAT</a></p> <p>I just wanted to thank this sub for all the cool ideas. I love it here.</p> </div><!-- SC_ON --> &#32; submitted by &#32; <a href=\"https://www.reddit.com/user/wholeywoolly\"> /u/wholeywoolly </a> <br/> <span><a href=\"https://www.reddit.com/r/MachineLearning/comments/94alf1/p_sonic_the_hedgehog_bot_with_neat_and_openai/\">[link]</a></span> &#32; <span><a href=\"https://www.reddit.com/r/MachineLearning/comments/94alf1/p_sonic_the_hedgehog_bot_with_neat_and_openai/\">[comments]</a></span>"; 
+
+       expect(Feed.htmlParser(content)).toBe(content);
+    });
 });
