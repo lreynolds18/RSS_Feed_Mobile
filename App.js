994247a71a276e2565e7c81838e007c6bfe13c82
Lucas Reynolds <lreynolds18@gmail.com>
2018-08-08 11:30:16 -0400

    Removed arrow function - I don't like the syntax.  Moved componentsWillMount to componentsDidMount because componentsWillMount is depreciated

--- App.js
+++ App.js
@@ -33,9 +33,11 @@ export default class App extends Component {
     RSS: []
   };
 
-  setRSS = (RSS) => this.setState({RSS});
+  setRSS(RSS) {
+      this.setState({RSS});
+  }
 
-  async componentWillMount() {
+  async componentDidMount() {
     await Expo.Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
